328e3f025e2271a87ea8ed0b43a44d901d84774c
alepalacio <alejandropalacio24@gmail.com>
2021-10-11 18:00:39 -0300

    creating first celery tasks

---
+++ core/app2/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.edit import FormView
+from app2.forms import ReviewForm
+
+
+# Create your views here.
+
+class ReviewEmailView(FormView):
+
+    template_name = 'review.html'
+    form_class = ReviewForm
+
+    def form_valid(self,form):
+        form.send_email()
+        message = 'Thanks for the review!'
+        return HttpResponse(message)
